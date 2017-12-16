913ccd8972098843e342ead231068ad5acb2a1aa
Vicente <pouyio@gmail.com>
2017-12-16 16:50:13 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,84 @@
+const express = require('express')
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const raw = require('./data.json');
+raw.blackCards = raw.blackCards.filter(e => e.pick === 1);
+
+app.use(express.static('.'))
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
+app.get('/card', (req, res) => res.send(raw.whiteCards[Math.floor(Math.random() * raw.whiteCards.length)]));
+
+let humans = [];
+
+const _drawCards = () => {
+    humans.map(h => {
+        const cards = Array(10)
+            .fill(1)
+            .map(e => raw.whiteCards[Math.floor(Math.random() * raw.whiteCards.length)])
+            .map((e, i) => ({ text: e, id: Buffer.from(e).toString('base64') }));
+        io.to(h.id).emit('white-cards', cards)
+    });
+}
+
+const _recalculateleader  = () => {
+    let position = humans.findIndex(h => h.isLeader) + 1;
+    if(position >= humans.length) position = 0;
+
+    humans = humans.map(h => {
+        h.isLeader = false;
+        return h;
+    });
+    humans[position].isLeader = true;
+    return humans;
+}
+
+const _newRownd = () => {
+    humans = _recalculateleader();
+    io.emit('new-round', raw.blackCards[Math.floor(Math.random() * raw.blackCards.length)].text, humans);
+}
+
+io.on('connection', (socket) => {
+
+    socket.emit('just-connected', humans, socket.id);
+
+    socket.on('enter-room', (nick) => {
+        humans.push({ nick, id: socket.id, counter: 0, isLeader: false, leaderCounter: 0 });
+        io.emit('enter-room', humans);
+    });
+
+    socket.on('disconnect', () => {
+        humans = humans.filter(h => h.id !== socket.id);
+        io.emit('leave-room', humans);
+        _newRownd();
+    });
+
+    socket.on('ready', () => {
+        const index = humans.findIndex(h => h.id === socket.id);
+        humans[index].ready = true;
+        const allReady = humans.filter(h => h.ready).length;
+        if (humans.length === allReady && allReady > 1) {
+            _drawCards();
+            _newRownd();
+        }
+    });
+
+    socket.on('card-selected', (cardId, cardtext) => {
+        const leader = humans.find(h => h.isLeader);
+        if (leader.id !== socket.id) {
+            io.to(leader.id).emit('card-selected', cardId, cardtext, humans.find(h => h.id === socket.id).nick);
+        }
+    });
+    
+    socket.on('round-win', (humanId) => {
+        const index = humans.findIndex(h => h.id === humanId);
+        humans[index].counter++;
+        io.to(humanId).emit('you-won');
+        _newRownd();
+    });
+
+});
+
+
+
+http.listen(3000, () => console.log('listening on *:3000'));
---
+++ index.js
@@ -0,0 +1,122 @@
+const socket = io();
+let humans = [];
+let leader;
+let myId;
+let whiteCards = [];
+const blackCard = document.getElementById('blackCard');
+
+const humanPlus = event => {
+    socket.emit('round-win', event.target.dataset.id);
+}
+
+const _updateHumans = (_friends) => {
+    humans = _friends;
+    leader = humans.find(h => h.isLeader);
+    const ul = document.getElementById('humans');
+    ul.innerHTML = '';
+    _friends.forEach(friend => {
+        const node = document.createElement('LI');
+        node.style.fontSize = friend.id === myId ? 'larger':'inherit';
+        node.style.fontWeight = friend.id === myId ? 'bold':'inherit';
+        let textnode = document.createTextNode(`${friend.id === myId ? '🔵': '⚫️'}️ ${friend.nick} : ${friend.counter}`);
+        
+        if(leader && (leader.id === friend.id)) {
+            textnode = document.createTextNode(`👑 ${friend.nick} : ${friend.counter}`);
+        } 
+        node.appendChild(textnode);
+
+        if(leader && leader.id === myId && friend.id !== myId) {
+            const nodePlus = document.createElement('BUTTON');
+            const textPlus = document.createTextNode(' 🦄 Win');
+            nodePlus.dataset.id = friend.id;
+            nodePlus.dataset.nick = friend.nick;
+            nodePlus.onclick = humanPlus;
+            nodePlus.appendChild(textPlus);
+            node.appendChild(nodePlus);
+        }
+
+        ul.appendChild(node);
+    });
+}
+
+const selectCard = async event => {
+    const cardId = event.target.dataset.id;
+    const cardtext = event.target.dataset.cardtext;
+    const cardLIToRemove = document.querySelector(`[data-id~="${cardId}"]`).parentNode;
+    cardLIToRemove.parentNode.removeChild(cardLIToRemove);
+    socket.emit('card-selected', cardId, cardtext);
+    document.getElementById('whiteCards').classList.add('disabled');
+}
+
+
+const _paintWhiteCards = () => {
+    const ul = document.getElementById('whiteCards');
+    ul.innerHTML = '';
+    whiteCards.forEach((whiteCard) => {
+        const nodeLi = document.createElement('LI');
+        const nodeBtn = document.createElement('BUTTON');
+        const btnnode = document.createTextNode(whiteCard.text);
+        nodeBtn.dataset.id = whiteCard.id;
+        nodeBtn.dataset.cardtext = whiteCard.text;
+        nodeBtn.onclick = selectCard;
+        nodeBtn.appendChild(btnnode);
+        nodeLi.appendChild(nodeBtn);
+        ul.appendChild(nodeLi);
+    });
+}
+
+const enterRoom = () => {
+    socket.emit('enter-room', document.getElementById('nick').value);
+    document.getElementById('form').hidden = true;
+    document.getElementById('ready').hidden = false;
+}
+
+const ready = () => {
+    socket.emit('ready');
+    document.getElementById('ready').hidden = true;
+}
+
+socket.on('enter-room', _updateHumans);
+socket.on('leave-room', _updateHumans);
+socket.on('just-connected', (humans, id)=> {
+    myId = id;
+    _updateHumans(humans);
+});
+
+socket.on('new-round', async (_blackCard, _humans) => {
+    humans = _humans;
+    _updateHumans(humans);
+    blackCard.innerHTML = _blackCard;
+    leader = humans.find(h => h.isLeader);
+    const ul = document.getElementById('selectedCards');
+    ul.innerHTML = '';
+    if(leader.id === myId) {
+        document.getElementById('whiteCards').classList.add('disabled');
+        document.getElementById('selectedCards').attributes.hidden = false;
+    }else {
+        document.getElementById('whiteCards').classList.remove('disabled');
+        document.getElementById('selectedCards').attributes.hidden = true;
+    }
+
+    while(whiteCards.length < 10) {
+        const newCard = await (await fetch('/card')).text();
+        whiteCards.push(newCard);
+    }
+    _paintWhiteCards();
+});
+
+socket.on('white-cards', (_whiteCards) => {
+    if(whiteCards.length) return;
+    whiteCards = _whiteCards;
+    _paintWhiteCards();
+});
+
+socket.on('you-won', () => alert('YOU WON THIS ROUND!'));
+
+socket.on('card-selected', (_cardId, cardtext, _friendNick) => {
+    const ul = document.getElementById('selectedCards');
+    const nodeCard = document.createElement('LI');
+    const textCard = document.createTextNode(`${_friendNick} - ${cardtext}`);
+    nodeCard.appendChild(textCard);
+    ul.appendChild(nodeCard);
+});
