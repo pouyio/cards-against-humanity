e986f2672ba0b63da47ba714c7d83a45dc4bc873
Vicente <pouyio@gmail.com>
2018-02-13 21:25:41 +0100

    hide name on  response

--- client/src/components/Human.js
+++ client/src/components/Human.js
@@ -15,11 +15,11 @@ export default class Human extends Component {
 
         const safeResponse = { __html: sanitizeHtml(this.props.human.response || '') };
 
-        const emojiTimer = <span role="img" aria-label="timer" className="rotate-forever" style={{ display: 'inline-block' }}>⏳</span>
+        const EmojiTimer = <span role="img" aria-label="timer" className="rotate-forever" style={{ display: 'inline-block' }}>⏳</span>
 
         return (
             <li className={'list-group-item card ' + responseClass}>
-                <div className={'text-capitalize' + (this.props.human.response ? ' small' : '')}>{this.props.human.nick} {!this.props.human.response ? emojiTimer : ''}</div>
+                {!this.props.human.response && EmojiTimer}
                 {this.props.human.response && <div className="card-body p-3" style={{ fontStyle: 'italic', fontSize: '1.2em' }} dangerouslySetInnerHTML={safeResponse} />}
                 {this.props.human.response &&
                     <div className="btn btn-outline-dark d-block" onClick={e => this.onSelected(this.props.human.id)}>
