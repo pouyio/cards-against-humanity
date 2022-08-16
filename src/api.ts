4c8bd7f5eb03b69680374d59515114d4e5025c58
pouyio <pouyio@gmail.com>
2022-08-16 19:57:58 +0200

    fix you-won message

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,4 @@
-const socket = new WebSocket("ws://localhost:3001/ws");
+export const socket = new WebSocket("ws://localhost:3001/ws");
 
 const actionHandlers = {};
 
@@ -6,7 +6,9 @@ const sendMessage = (action, ...data) => {
   if (socket.readyState === WebSocket.OPEN) {
     socket.send(JSON.stringify({ action, data: data.map(String) }));
   } else {
-    setTimeout(() => { sendMessage(action, ...data) }, 100)
+    setTimeout(() => {
+      sendMessage(action, ...data);
+    }, 100);
   }
 };
 
